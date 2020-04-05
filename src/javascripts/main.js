87f6f31aff8736144271e33e569013fbedffa29c
David Everett <davideverett1989@gmail.com>
2020-04-04 21:41:46 -0500

    Able to login and logout.

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,6 +3,7 @@ import apiKeys from './helpers/apiKeys.json';
 
 import auth from './components/auth/auth';
 import authData from './helpers/data/authData';
+import logout from './components/logout/logout';
 
 import '../styles/main.scss';
 import 'bootstrap';
@@ -11,6 +12,7 @@ const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   auth.printSignInButton();
   authData.checkLoginStatus();
+  logout.logoutEvent();
 };
 
 init();
