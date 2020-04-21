3f9034293ba13a0a2b92edcb28a355c75f7b44ff
David Everett <davideverett1989@gmail.com>
2020-04-21 18:04:26 -0500

    wip

--- src/javascripts/components/singleEntry/singleEntry.js
+++ src/javascripts/components/singleEntry/singleEntry.js
@@ -6,7 +6,10 @@ const makeEntryBox = (entry, thisEntryPlace) => {
   domString += `    <div id="${entry.id}" class="p-3 card bg-warning text-white">`;
   domString += '        <div class="d-inline-flex flex-column">';
   domString += `        <h2 class="m-2">${thisEntryPlace.title}</h2>`;
-  domString += '        <button class="entry-delete-button btn btn-danger align-self-end"><i class="fas fa-times"></i></button>';
+  domString += '        <div class="d-flex entry-button-group align-self-end">';
+  domString += '          <button class="m-2 entry-edit-button btn btn-success align-self-end"><i class="far fa-edit"></i></>';
+  domString += '          <button class="m-2 entry-delete-button btn btn-danger align-self-end"><i class="fas fa-times"></i></button>';
+  domString += '        </div>';
   domString += '        </div>';
   domString += `        <p class="m-2 lead">${entry.text}</p>`;
   domString += '    </div>';
